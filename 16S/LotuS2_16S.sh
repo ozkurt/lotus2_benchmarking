fd5a68c1d400c33714ff0204529322174fbf3557
ozkurt <ezgi140@gmail.com>
2022-01-01 21:51:55 +0000

    Update LotuS2_16S.sh
    LotuS2 cmds for the analysis of the 16S datasets.

--- 16S/LotuS2_16S.sh
+++ 16S/LotuS2_16S.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#Define the directories for the map ($MAP), input ($INP) and output ($OUTP)
 
+#For the soil-16S dataset, primers can be defined as a column in the mapping file or in the command: -forwardPrimer GTGYCAGCMGCCGCGGTAA -reversePrimer GGCCGYCAATTYMTTTRAGTTT
+
+methods="dada2 unoise uparse vsearch"
+sdm_params="sdm_miSeq.txt"
+
+
+
+for method in $methods; do
+        for param_path in $sdm_params; do
+            param_name="${param_path%.*}"
+            lotus2 -i $INP -o ${OUTP}_${param_name}_${method} -m $MAP -s $param_path -taxAligner lambda -CL $method -thr 12
+        done
+done        
