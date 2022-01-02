ad7d920f33bc3ab96ac49950ba8d990d83805a72
ozkurt <ezgi140@gmail.com>
2022-01-02 20:14:36 +0000

    Update LotuS2_16S.sh

--- 16S/LotuS2_16S.sh
+++ 16S/LotuS2_16S.sh
@@ -2,6 +2,7 @@
 #Define the directories for the map ($MAP), input ($INP) and output ($OUTP)
 
 #For the soil-16S dataset, primers can be defined as a column in the mapping file or in the command: -forwardPrimer GTGYCAGCMGCCGCGGTAA -reversePrimer GGCCGYCAATTYMTTTRAGTTT
+#-lulu was deactivated (-lulu 0) for the mock dataset.
 
 methods="dada2 unoise uparse vsearch"
 sdm_params="sdm_miSeq.txt"
