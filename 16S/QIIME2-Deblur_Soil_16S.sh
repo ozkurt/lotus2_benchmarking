4ee6b942c2553a029b8e863631644c187d3aaeb0
ozkurt <ezgi140@gmail.com>
2022-01-02 19:44:23 +0000

    Demultiplexing of the Soil-16S dataset
    "Demultiplexing" of the Soil-16S dataset slightly differs from the Gut-16S dataset. Please see QIIME2-Deblur_Soil_16S.sh for further information.
    "Denoising" and "Taxonomy" of the Gut- and Soil-16S datasets are performed using the same QIIME2 commands.

---
+++ 16S/QIIME2-Deblur_Soil_16S.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Define the path for the already demultiplexed reads ($INP):
+
+echo "Importing the already demultiplexed dataset:"
+qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' --input-path $INP --input-format CasavaOneEightSingleLanePerSampleDirFmt --output-path demux-soil.qza
+
+
+echo "Removing primers:"
+
+qiime cutadapt trim-paired \
+--i-demultiplexed-sequences demux-soil.qza \
+--p-front-f GTGYCAGCMGCCGCGGTAA \
+--p-front-r GGCCGYCAATTYMTTTRAGTTT \
+--p-cores 12 \
+--o-trimmed-sequences $INP/trimmed-seqs.qza \
+--verbose
+
+echo "Joining the paired-end reads:"
+qiime vsearch join-pairs --i-demultiplexed-seqs $INP/trimmed-seqs.qza --o-joined-sequences demux-soil-joined.qza
